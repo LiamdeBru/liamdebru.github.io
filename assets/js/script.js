ed17f25cf895129f5bd6b1eae88a3119bf981049
LiamdeBru <151225182+LiamdeBru@users.noreply.github.com>
2024-10-19 19:41:01 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -365,7 +365,7 @@ playerVolumeRange.addEventListener("input", changeVolume);
 const muteVolume = function () {
   if (!audioSource.muted) {
     audioSource.muted = true;
-    playerVolumeBtn.children[0].textContent = "volume_off";
+    playerVolumeBtn.children[0].textContent = "";
   } else {
     changeVolume();
   }
